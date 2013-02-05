100994ca7df41c25c31c0f7028a03934cac666eb
Victor Chahuneau <vchahune@cs.cmu.edu>
2013-02-05 00:15:08 -0500

    use pkg-config

--- setup.py
+++ setup.py
@@ -18,14 +18,26 @@ You should have received a copy of the GNU General Public License
 along with PyHunspell.  If not, see <http://www.gnu.org/licenses/>.
 """
 
+import sys
+import subprocess as sp
 from distutils.core import setup, Extension
+
+try:
+    cflags = sp.check_output('pkg-config --cflags hunspell '.split()).split()
+    ldflags = sp.check_output('pkg-config --libs hunspell '.split()).split()
+    for flag in ldflags:
+        if flag.startswith('-L'):
+            lib_dir = flag[2:]
+except sp.CalledProcessError:
+    sys.stderr.write('Cannot find pkg-config for hunspell.\n'
+    'Try setting the PKG_CONFIG_PATH variable.')
+    sys.exit(1)
 
 main = Extension('hunspell', 
 	define_macros = [('_LINUX',None)],
-	libraries = ['hunspell'],
-	include_dirs = ['/usr/include/hunspell'],
 	sources = ['hunspell.c'],
-	extra_compile_args = ['-Wall'])
+    extra_link_args = ldflags + ['-Wl,-rpath,'+lib_dir],
+	extra_compile_args = cflags + ['-Wall'])
 
 setup(name = "hunspell",
 	version = "0.1",
